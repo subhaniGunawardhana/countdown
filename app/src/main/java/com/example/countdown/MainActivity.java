78ac4a1afd3769bcd79f3c54a036a984dd33367e
subhani <subhanigunawardhanadap@gmail.com>
2019-03-06 15:21:58 +0530

    hasjqwhlgfwer;o

--- app/src/main/java/com/example/countdown/MainActivity.java
+++ app/src/main/java/com/example/countdown/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.countdown;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.logging.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        countdown();
+    }
+    private void countdown(){
+        TextView textView2 = findViewById(R.id.textView2);
+        Handler handler = new Handler();
     }
 }
